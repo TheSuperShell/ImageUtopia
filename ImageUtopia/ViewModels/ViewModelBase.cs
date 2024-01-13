e64722cac317b941814270d31d5da6e027f204a9
Steven Rudenko <steven.gucum@gmail.com>
2024-01-13 22:09:17 +0300

    Init the project

---
+++ ImageUtopia/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using ReactiveUI;
+
+namespace ImageUtopia.ViewModels;
+
+public class ViewModelBase : ReactiveObject
+{
+}
---
+++ ImageUtopia/Views/MainWindow.axaml.cs
@@ -0,0 +1,10 @@
+using Avalonia.Controls;
+
+namespace ImageUtopia.Views;
+
+public partial class MainWindow : Window
+{
+	public MainWindow() {
+		InitializeComponent();
+	}
+}
