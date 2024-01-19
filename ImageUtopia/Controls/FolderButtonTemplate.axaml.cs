d9528ee8224c997e9191b2f818924d81a137ae9d
Steven Rudenko <steven.gucum@gmail.com>
2024-01-19 16:14:24 +0300

    Add folder button template

---
+++ ImageUtopia/Controls/FolderButtonTemplate.axaml.cs
@@ -0,0 +1,23 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Controls.Primitives;
+
+namespace ImageUtopia.Controls;
+
+public class FolderButtonTemplate : TemplatedControl
+{
+	public static readonly StyledProperty<string> FolderNameProperty =
+		AvaloniaProperty.Register<FolderButtonTemplate, string>(nameof(FolderName), "Folder Name");
+	public static readonly StyledProperty<int> FolderCountProperty =
+		AvaloniaProperty.Register<FolderButtonTemplate, int>(nameof(FolderCount), 0);
+	
+	public string FolderName {
+		get => GetValue(FolderNameProperty);
+		set => SetValue(FolderNameProperty, value);
+	}
+	
+	public int FolderCount {
+		get => GetValue(FolderCountProperty);
+		set => SetValue(FolderCountProperty, value);
+	}
+}
--- ImageUtopia/ViewModels/MainWindowViewModel.cs
+++ ImageUtopia/ViewModels/MainWindowViewModel.cs
@@ -1,8 +1,18 @@
-﻿namespace ImageUtopia.ViewModels;
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace ImageUtopia.ViewModels;
 
 public class MainWindowViewModel : ViewModelBase
 {
 #pragma warning disable CA1822 // Mark members as static
 	public string Greeting => "Welcome to Avalonia!";
 #pragma warning restore CA1822 // Mark members as static
+
+	public ObservableCollection<string> FolderNames { get; } = new(new List<string>()
+	{
+		"Folder 1",
+		"Folder 2",
+		"Folder 3",
+	});
 }
