983c030dfbd1d605d364a7575343431e9ac1198d
Steven Rudenko <steven.gucum@gmail.com>
2024-01-21 15:26:05 +0300

    Add images grid

--- ImageUtopia/ViewModels/MainWindowViewModel.cs
+++ ImageUtopia/ViewModels/MainWindowViewModel.cs
@@ -9,10 +9,18 @@ public class MainWindowViewModel : ViewModelBase
 	public string Greeting => "Welcome to Avalonia!";
 #pragma warning restore CA1822 // Mark members as static
 
-	public ObservableCollection<string> FolderNames { get; } = new(new List<string>()
-	{
+	public ObservableCollection<string> FolderNames { get; } = new([
 		"Folder 1",
 		"Folder 2",
-		"Folder 3",
-	});
+		"Folder 3"
+	]);
+	
+	public ObservableCollection<string> ImageNames { get; } = new([
+		"Image 1",
+		"Image 2",
+		"Image 3",
+		"Cats",
+		"Dogs",
+		"Fish",
+	]);
 }
