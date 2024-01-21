a04b7be61218c877cdb9309909d9f75dc48676b1
Steven Rudenko <steven.gucum@gmail.com>
2024-01-21 22:37:47 +0300

    Change Folder buttons to ListBox

---
+++ ImageUtopia/Controls/FoldersListBox.axaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Avalonia.Controls;
+
+namespace ImageUtopia.Controls;
+
+public partial class FoldersListBox : ListBox
+{
+	public FoldersListBox() {
+		InitializeComponent();
+	}
+
+
+	protected override Type StyleKeyOverride => typeof(ListBox);
+}
--- ImageUtopia/ViewModels/MainWindowViewModel.cs
+++ ImageUtopia/ViewModels/MainWindowViewModel.cs
@@ -8,7 +8,13 @@ public class MainWindowViewModel : ViewModelBase
 #pragma warning disable CA1822 // Mark members as static
 	public string Greeting => "Welcome to Avalonia!";
 #pragma warning restore CA1822 // Mark members as static
-
+	
+	public ObservableCollection<string> MainFolderNames { get; } = new([
+		"All",
+		"Uncategorized",
+		"Random",
+	]);
+	
 	public ObservableCollection<string> FolderNames { get; } = new([
 		"Folder 1",
 		"Folder 2",
