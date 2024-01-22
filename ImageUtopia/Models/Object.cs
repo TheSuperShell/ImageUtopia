ceaa19117aafb3a5d9c996fd8ca7f5d0c31f8615
Steven Rudenko <steven.gucum@gmail.com>
2024-01-22 21:31:33 +0300

    Add Image Services

--- ImageUtopia/Models/Object.cs
+++ ImageUtopia/Models/Object.cs
@@ -1,3 +1,3 @@
 ﻿namespace ImageUtopia.Models;
 
-public record Object(string Path, string Extension);
+public record Object(string Name, string Path, string Extension,  ulong Size, bool IsImage, (uint, uint) Dimensions);
---
+++ ImageUtopia/Services/ImageServices.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using ImageUtopia.Models;
+
+namespace ImageUtopia.Services;
+
+public class ImageServices
+{
+	public async Task<Object[]> LoadImagesAsync(string path) =>
+		await Task.Run(() => 
+			Directory.GetFiles(path)
+				.Select(filePath => new MetadataExtractor(filePath))
+				.Where(extractor => extractor.IsValidFile)
+				.Select(extractor => new Object(
+					extractor.Name, 
+					extractor.Path,
+					extractor.Extension,
+					extractor.Size,
+					extractor.IsImage,
+					extractor.Dimensions)
+				).ToArray());
+}
