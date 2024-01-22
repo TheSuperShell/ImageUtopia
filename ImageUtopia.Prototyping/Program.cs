656ae18c55a0b658551fe2cfc468dd9a90b55a58
Steven Rudenko <steven.gucum@gmail.com>
2024-01-22 20:17:16 +0300

    Add Image loader prototype and a prototyping environment

---
+++ ImageUtopia.Prototyping/Program.cs
@@ -0,0 +1,8 @@
+﻿using ImageUtopia.Models;
+
+var files = new ImageLoader(@"C:\Users\User\Documents\NET Projects\ImageDBApp\TestImages");
+var images = files.GetImages();
+foreach (var image in images)
+{
+	Console.WriteLine(image);
+}
---
+++ ImageUtopia/Models/ImageLoader.cs
@@ -0,0 +1,13 @@
+﻿using System.IO;
+
+namespace ImageUtopia.Models;
+
+public class ImageLoader(string path)
+{
+	private string _path = path;
+	
+	public string[] GetImages()
+	{
+		return Directory.GetFiles(_path);
+	}
+}
