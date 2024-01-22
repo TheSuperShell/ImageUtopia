4ab938056db6478804fc45bb9d0a7168f45c9bb1
Steven Rudenko <steven.gucum@gmail.com>
2024-01-22 21:31:50 +0300

    Prototype test image services

--- ImageUtopia.Prototyping/Program.cs
+++ ImageUtopia.Prototyping/Program.cs
@@ -1,8 +1,21 @@
-﻿using ImageUtopia.Models;
+﻿using ImageUtopia.Services;
+using Object = ImageUtopia.Models.Object;
 
-var files = new ImageLoader(@"C:\Users\User\Documents\NET Projects\ImageDBApp\TestImages");
-var images = files.GetImages();
-foreach (var image in images)
+const string path = @"C:\Users\User\Documents\NET Projects\ImageUtopiaApp\TestImages";
+
+var images = await new ImageServices().LoadImagesAsync(path);
+
+foreach (Object image in images)
 {
-	Console.WriteLine(image);
+	Console.WriteLine($"Name: {image.Name}");
+	Console.WriteLine($"Path: {image.Path}");
+	Console.WriteLine($"Extension: {image.Extension}");
+	Console.WriteLine($"Size: {image.Size}");
+	Console.WriteLine($"IsImage: {image.IsImage}");
+	Console.WriteLine($"Dimensions: {image.Dimensions}");
+	Console.WriteLine();
 }
+
+
+
+
