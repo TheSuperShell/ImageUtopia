46f76b23160fc26a27e62b082adc20dab740c1b3
Steven Rudenko <steven.gucum@gmail.com>
2024-01-22 19:43:46 +0300

    Add utils
    Add Option and Result structs
    Add unit tests for them

---
+++ ImageUtopia/Utils/Option.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace ImageUtopia.Utils;
+
+public readonly struct Option<T>
+{
+	private readonly T? _value;
+
+	private Option(T? value) {
+		_value = value;
+	}
+
+	public static Option<T> Some(T value) => new(value);
+	public static Option<T> None() => new(default);
+
+	public bool IsSome => _value != null;
+	public bool IsNone => _value == null;
+
+	public T Unwrap() => _value switch
+	{
+		null => throw new InvalidOperationException("Option is empty"),
+		_ => _value
+	};
+	
+	public T UnwrapOrDefault(T defaultValue) => _value switch
+	{
+		null => defaultValue,
+		_ => _value
+	};
+	
+	public T Expect(string message) => _value switch
+	{
+		null => throw new InvalidOperationException(message),
+		_ => _value
+	};
+	
+	public Option<TO> Map<TO>(Func<T, TO> mapper) => _value switch
+	{
+		null => Option<TO>.None(),
+		_ => Option<TO>.Some(mapper(_value)),
+	};
+
+}
---
+++ ImageUtopia/Utils/Result.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace ImageUtopia.Utils;
+
+public readonly struct Result<T, TE> where TE : Exception
+{
+	private readonly T? _result;
+	private readonly TE? _exception;
+	
+	private Result(T? result, TE? exception) {
+		_result = result;
+		_exception = exception;
+	}
+	
+	public static Result<T, TE> Ok(T result) => new(result, default);
+	public static Result<T, TE> Err(TE exception) => new(default, exception);
+	
+	public bool IsOk => _result != null;
+	public bool IsErr => _result == null;
+	
+	public T Unwrap() => (_result, _exception) switch
+	{
+		(null, not null) => throw _exception,
+		(not null, null) => _result,
+		(_, _) => throw new UnreachableException()
+	};
+	
+	public T UnwrapOrDefault(T defaultValue) => (_result, _exception) switch
+	{
+		(null, not null) => defaultValue,
+		(not null, null) => _result,
+		(_, _) => throw new UnreachableException()
+	};
+}
