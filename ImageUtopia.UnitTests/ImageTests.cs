7f00b97f55b209eea9ab493e4c8486926e44c0d1
Steven Rudenko <steven.gucum@gmail.com>
2024-01-24 17:50:55 +0300

    I forgot to add this 2 files to previous 2 commits :(

---
+++ ImageUtopia.UnitTests/ImageTests.cs
@@ -0,0 +1,39 @@
+﻿using ImageUtopia.Models;
+using Object = ImageUtopia.Models.Object;
+
+namespace ImageUtopia.UnitTests;
+
+public class ImageTests
+{
+	[Test]
+	public void ImageMetadataTest()
+    {
+	    string path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "TestImages", "unnamed.jpg");
+	    TestContext.WriteLine(path);
+	    
+        var objectBuilder = new ObjectBuilder(path);
+		var result = objectBuilder.Init().Build();
+		Assert.That(result.IsOk, Is.True);
+		
+		Object image = result.Unwrap();
+        Assert.Multiple(() =>
+        {
+            Assert.That(image.Name, Is.EqualTo("unnamed.jpg"));
+            Assert.That(image.Extension, Is.EqualTo(".jpg"));
+            Assert.That(image.Size, Is.EqualTo(40545));
+            Assert.That(image.IsImage, Is.True);
+            Assert.That(image.Dimensions, Is.EqualTo((640, 640)));
+        });
+    }
+
+	[Test]
+	public void ImageBuilderErrorsTest() {
+		var objectBuilder = new ObjectBuilder("nonexistent.jpg");
+		var result = objectBuilder.Init().Build();
+		
+		Assert.Multiple(() => {
+			Assert.That(result.IsErr, Is.True);
+			Assert.That(result.UnwrapErr().Message, Is.EqualTo("File not found"));
+		});
+	}
+}
---
+++ ImageUtopia/Utils/ResultExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace ImageUtopia.Utils;
+
+public static class ResultExtensions
+{
+	public static IEnumerable<T> ResultOrDebug<T, TE>(this IEnumerable<Result<T, TE>> results) where T : class where TE : Exception
+	{
+		foreach (var result in results)
+		{
+			if (result.IsOk) {
+				yield return result.Unwrap();
+			} else {
+				Debug.WriteLine(result.UnwrapErr().Message);
+			}
+		}
+	}
+}
